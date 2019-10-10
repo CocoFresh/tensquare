6020e69503c426e3c3347cfae24476fb9592706a
xintao <291954264@qq.com>
2019-10-10 11:51:51 +0800

    tensquare

---
+++ tensquare_base/src/main/java/com/tensquare/base/BaseApplication.java
@@ -0,0 +1,23 @@
+package com.tensquare.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import util.IdWorker;
+
+/**
+ * @Auther: xintao.feng
+ * @Date: 2019/10/10 10:53
+ * @Description:
+ */
+@SpringBootApplication
+public class BaseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BaseApplication.class, args);
+    }
+
+    @Bean
+    public IdWorker idWorker() {
+        return new IdWorker(1, 1);
+    }
+}
