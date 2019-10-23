3d42af4ad3532500ef5decc8c33b6ac2b6a5a950
xintao <291954264@qq.com>
2019-10-23 14:42:05 +0800

    springcloud

---
+++ tensquare_config/src/main/java/com/tensquare/config/ConfigApplication.java
@@ -0,0 +1,18 @@
+package com.tensquare.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Auther: xintao.feng
+ * @Date: 2019/10/23 11:05
+ * @Description:
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
